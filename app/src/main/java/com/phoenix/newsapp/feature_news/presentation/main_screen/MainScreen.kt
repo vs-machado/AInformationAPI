1a0a9a17475728212a5c7d381f3c56e6737084c7
vs-machado <vinicius.s.machado@protonmail.com>
2024-10-22 15:17:04 -0300

    Added an app logo to the MainScreen. Replaced SearchBar by a custom implementation with BasicTextField.

--- app/src/main/java/com/phoenix/newsapp/feature_news/presentation/main_screen/MainScreen.kt
+++ app/src/main/java/com/phoenix/newsapp/feature_news/presentation/main_screen/MainScreen.kt
@@ -1,53 +1,55 @@
 package com.phoenix.newsapp.feature_news.presentation.main_screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.phoenix.newsapp.R
 import com.phoenix.newsapp.feature_news.domain.model.RssItem
 import com.phoenix.newsapp.feature_news.presentation.main_screen.components.FeedItem
+import com.phoenix.newsapp.feature_news.presentation.main_screen.components.SearchBar
 
-/**k
+/**
  * The main screen shows a search bar to allow user to query specific news.
  * A news feed is automatically shown if the fetch was successful,
  * if there is an error during the fetch, it displays an error and
  * show a "Try again" button.
  */
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     viewModel: MainScreenViewModel = hiltViewModel(),
@@ -55,90 +57,89 @@ fun MainScreen(
     onNavigationToAISummaryScreen: (RssItem, String) -> Unit
 ) {
     val feedState by viewModel.filteredFeed.collectAsState()
-    var searchText by rememberSaveable { mutableStateOf("") }
-    var expanded by rememberSaveable { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
-            SearchBar(
-                inputField = {
-                    SearchBarDefaults.InputField(
-                        query = searchText,
-                        onQueryChange = {
-                            searchText = it
-                            viewModel.updateSearchQuery(it)
-                        },
-                        onSearch = { 
-                            expanded = false 
-                            viewModel.updateSearchQuery(searchText)
-                       },
-                        expanded = false,
-                        onExpandedChange = { expanded = false},
-                        placeholder = { Text("Search news") },
-                        leadingIcon = {
-                            IconButton(onClick = {}) {
-                                Icon(
-                                    imageVector = Icons.Default.Search,
-                                    contentDescription = "Search"
-                                )
-                            }
-                        },
-                        trailingIcon = {
-                            if(searchText.isNotEmpty()){
-                                IconButton(onClick = {
-                                    searchText = ""
-                                    viewModel.updateSearchQuery("")
-                                }){
-                                    Icon(
-                                        imageVector = Icons.Default.Clear,
-                                        contentDescription = "Clear search"
-                                    )
-                                }
-                            }
-                        }
-                    )
-                },
-                expanded = false,
-                onExpandedChange = { expanded = false },
-                content = {},
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            )
+                    .background(color = Color.Black)
+                    .padding(
+                        top = WindowInsets.systemBars
+                            .asPaddingValues()
+                            .calculateTopPadding()
+                    )
+                    .padding(top = 16.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.black_logo),
+                    contentDescription = "App logo",
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .height(24.dp)
+                        .width(200.dp)
+                )
+            }
         }
     ) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            when(val state = feedState) {
-                is FeedState.Initial -> {}
-                is FeedState.Loading -> LoadingIndicator()
-                is FeedState.Success -> {
-                    FeedContent(
-                        feed = state.items,
-                        onNavigationToNewsDetail = onNavigationToNewsDetail,
-                        onNavigationToAISummaryScreen = onNavigationToAISummaryScreen
-                    )
-                }
-                is FeedState.Error -> {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Text(
-                            text = "Connection error occurred.",
-                            modifier = Modifier.padding(bottom = 16.dp)
-                        )
-                        Button(
-                            onClick = { viewModel.fetchRssFeed(isScrolling = false) }
-                        ) {
-                            Text("Try Again")
-                        }
-                    }
 
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(color = Color.Black)
+        ) {
+            var searchQuery by remember { mutableStateOf("") }
+
+            SearchBar(
+                onSearchTextChanged = { newQuery ->
+                    searchQuery = newQuery
+                },
+                horizontalPadding = 16.dp,
+                verticalPadding = 8.dp,
+                backgroundColor = Color(0xFF242B31),
+                cursorBrushColor = SolidColor(Color(0xFF20D16E)),
+                handleColor = Color(0xFF20D16E)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Box(
+                modifier = Modifier
+                    .background(Color.Black)
+                    .fillMaxSize()
+            ) {
+                when(val state = feedState) {
+                    is FeedState.Initial -> {}
+                    is FeedState.Loading -> LoadingIndicator()
+                    is FeedState.Success -> {
+                        val filteredFeed = state.items.filter {
+                            it.title.contains(searchQuery, ignoreCase = true) ||
+                                    it.description.contains(searchQuery, ignoreCase = true)
+                        }
+
+                        FeedContent(
+                            feed = filteredFeed,
+                            onNavigationToNewsDetail = onNavigationToNewsDetail,
+                            onNavigationToAISummaryScreen = onNavigationToAISummaryScreen
+                        )
+                    }
+                    is FeedState.Error -> {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text(
+                                text = "Connection error occurred.",
+                                modifier = Modifier.padding(bottom = 16.dp)
+                            )
+                            Button(
+                                onClick = { viewModel.fetchRssFeed(isScrolling = false) }
+                            ) {
+                                Text("Try Again")
+                            }
+                        }
+
+                    }
                 }
             }
         }
@@ -179,7 +180,7 @@ fun FeedContent(
                 text = "Latest news",
                 fontWeight = FontWeight.Medium,
                 fontSize = 24.sp,
-                modifier = Modifier.padding(start = 20.dp, top = 16.dp)
+                modifier = Modifier.padding(start = 20.dp, top = 8.dp)
             )
         }
         items(feed) { item ->
